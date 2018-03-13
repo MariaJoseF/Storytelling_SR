b15b832353f4b3d6e2fb0c894ae5b2a8b58b48eb
RParadeda <paradeda@gmail.com>
2018-03-13 11:32:54 +0100

    Audio added. Not final audio but its possible to test.

--- SOP/StoryHandler.cs
+++ SOP/StoryHandler.cs
@@ -238,7 +238,6 @@ namespace StoryOfPersonality
             Random random = new Random();
             int n = random.Next(0, 10);
             return n;
-
         }
 
         //internal string GetRightTag()
@@ -280,6 +279,7 @@ namespace StoryOfPersonality
             }
             else { return "0"; }
         }
+
         internal string GetDecisionUtteranceCategory()
         {
             return storyNodes[currentStoryNodeId].Before;
@@ -307,6 +307,7 @@ namespace StoryOfPersonality
             }
             return "<prosody volume=" + value + utterance + "</prosody>";
         }
+
         private String encloseWithRateTag(String utterance, Rate rate)
         {
             String value = "1"; // default
